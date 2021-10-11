854804eb47df32c45681dec733ac9017ff0105a0
tariksetia <setia.tarik@gmail.com>
2021-10-11 17:01:18 +0530

    DE-2591: added fx for matching time

--- task_script_utils/datetime_parser/datetime_info.py
+++ task_script_utils/datetime_parser/datetime_info.py
@@ -40,6 +40,7 @@ class DateTimeInfo:
     def get_checks(self):
         methods_dict = {
             "iana_tz": self.match_iana_tz,
+            "time": self.match_time,
         }
         return methods_dict
 
@@ -47,3 +48,31 @@ class DateTimeInfo:
         if token in pendulum.timezones:
             return token
 
+    def match_time(self, token: str):
+        pattern = r"\d+:\d+:\d+.\d+|\d+:\d+:\d+"
+        matches = re.findall(pattern, token)
+        if not matches:
+            return None
+
+        if len(matches) > 1:
+            raise MultipleTimesFoundError(
+                f"Multiple Time values found: {matches}")
+        time_ = matches[0].split(":")
+        hour, minutes, seconds = time_
+        if "." in seconds:
+            seconds, milliseconds = seconds.split(".")
+        else:
+            milliseconds = None
+
+        assert (0 <= int(hour) <= 24), InvalidTimeError(
+            f"Invalid time : {time_}. Hours value is incorrect")
+        assert (0 <= int(minutes) <= 60), InvalidTimeError(
+            f"Invalid time : {time_}. Minutes value is incorrect")
+        assert (0 <= int(seconds) <= 60), InvalidTimeError(
+            f"Invalid time : {time_}. Seconds value is incorrect")
+
+        self.hour = int(hour)
+        self.minutes = int(minutes)
+        self.seconds = int(seconds)
+        self.milliseconds = int(milliseconds)
+        return ':'.join(time_)
