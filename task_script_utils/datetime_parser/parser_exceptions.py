a1bc04873336ac64485fd93fc5f607ed75f1fa73
tariksetia <setia.tarik@gmail.com>
2021-10-11 16:52:11 +0530

    DE-2591: added exceptions classes

---
+++ task_script_utils/datetime_parser/parser_exceptions.py
@@ -0,0 +1,23 @@
+class InvalidYearError(Exception):
+    pass
+
+class AmbiguousDateError(Exception):
+    pass
+
+class MultipleDatesError(Exception):
+    pass
+
+class MultipleTimesFoundError(Exception):
+    pass
+
+class InvalidTimeError(Exception):
+    pass
+
+class InvalidOffsetError(Exception):
+    pass
+
+class MultipleOffsetsError(Exception):
+    pass
+
+class InvalidDateError(Exception):
+    pass
