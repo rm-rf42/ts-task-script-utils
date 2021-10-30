cb8edeada53d1400b66d550a76b7e000aaca85f5
tariksetia <setia.tarik@gmail.com>
2021-10-31 01:08:20 +0530

    DE-2591: added unit test file

---
+++ __tests__/unit/test_convert_to_iso.py
@@ -0,0 +1,37 @@
+import pytest
+from task_script_utils.datetime_parser import (
+    convert_to_ts_iso8601,
+    PipelineConfig,
+)
+
+config_with_fold_test_cases = {
+    ("2021-11-07T01:30:00 America/New_York", 0, "2021-11-07T05:30:00Z"),
+    ("2021-11-07T01:30:00 America/New_York", 1, "2021-11-07T06:30:00Z"),
+    ("2021-11-07T01:30:00.032 America/New_York", 1, "2021-11-07T06:30:00.032000Z"),
+
+    #TODO: Confirm these cases
+    ("2021-11-07T04:30:00.032 America/New_York", 1, "2021-11-07T09:30:00.032000Z"),
+    ("2021-11-07T04:30:00.032 America/New_York", 0, "2021-11-07T09:30:00.032000Z")
+}
+convert_to_iso_cases = {
+    ("2021-11-21T01:30:00 America/New_York", "2021-11-21T06:30:00Z"),
+    ("2021-11-21T01:30:00", "2021-11-21T01:30:00"),
+}
+
+@pytest.mark.parametrize(
+    "input_, fold, expected",
+    config_with_fold_test_cases
+)
+def test_convert_to_iso_with_fold(input_, fold, expected):
+    config = PipelineConfig(fold=fold, day_first=False)
+    result = convert_to_ts_iso8601(input_, config=config)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "input_, expected",
+    convert_to_iso_cases
+)
+def test_convert_to_iso(input_, expected):
+    result = convert_to_ts_iso8601(input_, config=PipelineConfig())
+    assert result == expected
