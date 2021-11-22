1c84f702d853c725d8e000f483417f9745784d8e
tariksetia <setia.tarik@gmail.com>
2021-11-22 16:34:27 +0530

    DE-2591: updated test cases

--- __tests__/unit/test_convert_to_iso.py
+++ __tests__/unit/test_convert_to_iso.py
@@ -28,7 +28,6 @@ fractional_seconds_test_cases = {
     ("2021-11-07T04:30:00.00123456 America/New_York","2021-11-07T09:30:00.00123456Z"),
     ("2021-11-07T04:30:00.01 America/New_York","2021-11-07T09:30:00.01Z"),
     ("2021-11-07T04:30:00.010 America/New_York","2021-11-07T09:30:00.010Z"),
-    # Error Case as length of subseconds > 7
     ("2021-11-07T04:30:00.1234567 America/New_York", '2021-11-07T09:30:00.1234567Z'),
 }
 
