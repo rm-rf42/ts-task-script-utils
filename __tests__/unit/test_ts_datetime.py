e074b5f370bfba608a927e24220b9918b016ba2b
tariksetia <setia.tarik@gmail.com>
2021-12-13 18:01:29 +0530

    DE-2591: minor updates

--- __tests__/unit/test_ts_datetime.py
+++ __tests__/unit/test_ts_datetime.py
@@ -6,33 +6,34 @@ from task_script_utils.datetime_parser.utils import from_pendulum_format
 
 
 subseconds_test_cases = [
+    # raw, TSDatetime.isofromat(), TSDatetime.datetime.isoformat()
     (
-        'Sunday, May 26th 2013 12:12:12.1 AM Asia/Kolkata',
+        "Sunday, May 26th 2013 12:12:12.1 AM Asia/Kolkata",
         "2013-05-26T00:12:12.1+05:30",
         "2013-05-26T00:12:12.100000+05:30"
     ),
     (
-        'Sunday, May 26th 2013 12:12:12.0001 AM Asia/Kolkata',
+        "Sunday, May 26th 2013 12:12:12.0001 AM Asia/Kolkata",
         "2013-05-26T00:12:12.0001+05:30",
         "2013-05-26T00:12:12.000100+05:30"
     ),
     (
-        'Sunday, May 26th 2013 12:12:12.00100 AM Asia/Kolkata',
+        "Sunday, May 26th 2013 12:12:12.00100 AM Asia/Kolkata",
         "2013-05-26T00:12:12.00100+05:30",
         "2013-05-26T00:12:12.001000+05:30"
     ),
     (
-        'Sunday, May 26th 2013 12:12:12.01010001 AM Asia/Kolkata',
+        "Sunday, May 26th 2013 12:12:12.01010001 AM Asia/Kolkata",
         "2013-05-26T00:12:12.01010001+05:30",
         "2013-05-26T00:12:12.010100+05:30",
     ),
     (
-        'Sunday, May 26th 2013 12:12:12.00120200 AM Asia/Kolkata',
+        "Sunday, May 26th 2013 12:12:12.00120200 AM Asia/Kolkata",
         "2013-05-26T00:12:12.00120200+05:30",
         "2013-05-26T00:12:12.001202+05:30"
     ),
     (
-        'Sunday, May 26th 2013 12:12:12.10000100 AM Asia/Kolkata',
+        "Sunday, May 26th 2013 12:12:12.10000100 AM Asia/Kolkata",
         "2013-05-26T00:12:12.10000100+05:30",
         "2013-05-26T00:12:12.100001+05:30"
     ),
